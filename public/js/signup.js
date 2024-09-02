4f769b96d60f0e5dd110784075da226572c2dfec
LeonathaTurtle <mancillh@gmail.com>
2024-09-02 18:07:55 -0500

    created comment form, started writing logic for comment, logic for comment not yet tested

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,14 +1,16 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#name-signup').value.trim();
+    const first_name = document.querySelector('#first-name-signup').value.trim();
+    const last_name = document.querySelector('#last-name-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (name && email && password) {
+    if (first_name && last_name && username && email && password) {
       const response = await fetch('/api/users/signup', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ first_name, last_name, username, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
