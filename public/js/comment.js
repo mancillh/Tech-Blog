95351fa052649754d80648adf59ac3567c1d88a1
LeonathaTurtle <mancillh@gmail.com>
2024-09-18 14:30:45 -0500

    removed excess code that wasn't needed from comment.js

--- public/js/comment.js
+++ public/js/comment.js
@@ -24,26 +24,6 @@ const commentFormHandler = async (event) => {
     }
   };
 
-  // const delButtonHandler = async (event) => {
-  //   if (event.target.hasAttribute('data-id')) {
-  //     const id = event.target.getAttribute('data-id');
-  
-  //     const response = await fetch(`/api/comments/${id}`, {
-  //       method: 'DELETE',
-  //     });
-  
-  //     if (response.ok) {
-  //       document.location.replace('/dashboard');
-  //     } else {
-  //       alert('Failed to delete comment');
-  //     }
-  //   }
-  // };
-
 document
 .querySelector('.actual-comment-form')
 .addEventListener('submit', commentFormHandler);
-
-// document
-// .querySelector('.posted-comment')
-// .addEventListener('click', delButtonHandler);
